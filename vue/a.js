f39cb3afc82940df2bbd5e9b298c5a50ec842fd7
Johnson Cheung <johnsoncheunghk06@yahoo.com.hk>
2016-08-13 13:36:27 +0800

    Commit

--- vue/a.js
+++ vue/a.js
@@ -1,18 +1,17 @@
 var aa = {};
 (function a(Vue,aa) {
     'use strict';
-    var vueOpt = {
-        el : "#body",
-        data: {
-            chkBox: [],
-            a: 1,
-            b: 2,
-            options: [
+    var chkBox = [];
+    var a = 1;
+    var b = 2;
+    var options = [
               { text: 'One', value: 'A' },
               { text: 'Two', value: 'B' },
               { text: 'Three', value: 'C' }
-            ]
-        },
+    ]
+    var vueOpt = {
+        el : "#body",
+        data: { chkBox, a,b,options},
         computed: {
             c: function c() {
                 return this.a+this.b;
